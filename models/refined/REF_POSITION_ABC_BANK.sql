9bf5a57d9c9b871b13d9ae99ed56056bfab6b7cc
Marc Arguijo <marcos.a.arguijo@gmail.com>
2024-10-17 22:08:48 +0000

    Formatting

--- models/refined/REF_POSITION_ABC_BANK.sql
+++ models/refined/REF_POSITION_ABC_BANK.sql
@@ -1,9 +1,10 @@
 WITH
 
 current_from_snapshot as (
-        SELECT *
-        FROM {{ ref('SNSH_ABC_BANK_POSITION') }}
-WHERE DBT_VALID_TO is null
+    SELECT 
+        *
+    FROM {{ ref('SNSH_ABC_BANK_POSITION') }}
+    WHERE DBT_VALID_TO is null
     )
 
 SELECT
